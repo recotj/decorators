6f486d569b0e09988dbda1d4e395e9f203bbe9b4
reco <recotj@gmail.com>
2016-04-28 15:45:41 +0800

    add decorator-guard

---
+++ packages/decorator-guard/src/index.js
@@ -0,0 +1,14 @@
+module.exports = ({precondition}) => (target, key, descriptor) => {
+	const method = descriptor.value;
+
+	if (typeof method !== 'function') return descriptor;
+	if (typeof precondition !== 'function') return descriptor;
+
+	descriptor.value = function () {
+		if (Reflect.apply(precondition, this, arguments)) {
+			Reflect.apply(method, this, arguments);
+		}
+	};
+
+	return descriptor;
+};
